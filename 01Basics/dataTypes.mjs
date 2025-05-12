0cdadc6d2b60dbd9860ed825bf641b5eabd47284
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-05-13 03:05:50 +0530

    primitive DT

--- 01Basics/dataTypes.mjs
+++ 01Basics/dataTypes.mjs
@@ -2,3 +2,11 @@
 var n = 9;
 let m = 10; // can change,  no redeclaration
 const p = 12; // can't change
+
+// DataTypes
+// Primtive DT
+const num = 12345; // Number
+const str = 'DarkSoul' // String
+const bool = true || false; // Boolean
+const noValue = null; // null
+let notAssigned; // undefined (remember to use 'let')
