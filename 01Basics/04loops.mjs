b68589eccb91507719b7ecb0997cb727164c5e66
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-05-13 22:51:08 +0530

    loops

---
+++ 01Basics/04loops.mjs
@@ -0,0 +1,19 @@
+
+// for loop
+for (let index = 0; index < 10; index++) {
+   // console.log(index);
+}
+
+// while loop
+let i = 0;
+while (i < 5) {
+   // console.log(`This is the ${i}th iteration`);
+   i++;
+}
+
+// do-while 
+let j = 0;
+do {
+   console.log(`You are in do-while loop for ${j}th time`);
+   j++;
+} while (j < 5);
