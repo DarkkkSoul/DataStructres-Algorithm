571a0392c076ff7fdeeb1c7458696a2e8b3327f3
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-05-16 14:08:43 +0530

    array looping

--- 01Concepts/Day02/01arrays.mjs
+++ 01Concepts/Day02/01arrays.mjs
@@ -1,5 +1,5 @@
 // Stores ordered collections of items, Which we can perform different operations on.
-// Two ways of declaring an array, arrays can contain different data types
+// Two ways of declaring an array, arrays can contain different data types.
 
 const arr1 = []; // literal
 const arr2 = new Array(); // constructor
@@ -12,6 +12,8 @@ const fruits = ['Mango', 'Orange', 'Apple'];
 console.log(fruits[0]);
 console.log(fruits[1]);
 console.log(fruits[2]);
+console.log('\n');
+
 
 // replace & add new elements
 fruits[0] = 'Strawberry';
@@ -19,10 +21,25 @@ fruits[3] = 'Mango';
 
 console.log(fruits[0]);
 console.log(fruits[3]);
+console.log('\n');
+
 
 console.log(fruits.length); // total elements
 console.log(fruits);
+console.log('\n');
+
 
 console.log(fruits[fruits.length - 1]);
 console.log(fruits.at(-1));
+console.log('\n');
 
+
+// Looping in array
+const Movies = ['Snow White', 'Mufasa', 'Frozen', 'Flow', 'Lilo & Stitch'];
+// can use forLoop
+
+// For - of loop
+for (const movie of Movies) {
+   console.log(movie);
+
+}
